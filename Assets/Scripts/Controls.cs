89e39732ddec821cbc4acce55d8399cfe33de7d3
Weedzard <arndslol@gmail.com>
2023-01-21 16:51:37 +0100

    Animations

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -7,6 +7,7 @@ public class Controls : MonoBehaviour
     //Movement speed
     private float movementSpeed = 5;
     public GameObject interactionPoint;
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
@@ -22,24 +23,32 @@ public class Controls : MonoBehaviour
             //Movement
             if (Input.GetKey(KeyCode.D))
             {
+                animator.SetBool("MovingRight", true);
                 transform.Translate(new Vector2(movementSpeed * Time.deltaTime, 0));
                 interactionPoint.transform.position = new Vector2(transform.position.x + 0.6f, transform.position.y);
             }
+            else { animator.SetBool("MovingRight", false); }
             if (Input.GetKey(KeyCode.A))
             {
+                animator.SetBool("MovingLeft", true);
                 transform.Translate(new Vector2(-movementSpeed * Time.deltaTime, 0));
                 interactionPoint.transform.position = new Vector2(transform.position.x + -0.6f, transform.position.y);
             }
+            else { animator.SetBool("MovingLeft", false); }
             if (Input.GetKey(KeyCode.W))
             {
+                animator.SetBool("MovingUp", true);
                 interactionPoint.transform.position = new Vector2(transform.position.x, transform.position.y + 0.6f);
                 transform.Translate(new Vector2(0, movementSpeed * Time.deltaTime));
             }
+            else { animator.SetBool("MovingUp", false); }
             if (Input.GetKey(KeyCode.S))
             {
+                animator.SetBool("MovingDown", true);
                 transform.Translate(new Vector2(0, -movementSpeed * Time.deltaTime));
                 interactionPoint.transform.position = new Vector2(transform.position.x, transform.position.y - 0.6f);
             }
+            else { animator.SetBool("MovingDown", false); }
         }
        
     }
