f83b9bc9ce7e2873cf5c2d78e1a0e797c16f0630
Theran1 <addg98@gmail.com>
2023-01-21 20:23:05 +0100

    Starting with dialogues

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -13,10 +13,10 @@ public class Interactable : MonoBehaviour
     public enum CharacterDialogue
     {
         Eyun,
-        Karl,
-        Ivori,
-        Ming,
-        Kleon,
+        Karl1,
+        Ivori1,
+        Ming1,
+        Kleon1,
     }
     public CharacterDialogue chosenDialogue;
 
@@ -59,20 +59,27 @@ public class Interactable : MonoBehaviour
         {
             case CharacterDialogue.Eyun:
                 return null;
-            case CharacterDialogue.Karl:
-                string localName = "";
-                Monologue fine = new Monologue("Kleon", "That's pog");
-                Monologue notFine = new Monologue(localName, "That's sadge :(");
+            case CharacterDialogue.Karl1:
+                string localName = "Karl";
+                Monologue response1 = new Monologue(localName, "That's pog");
+                Monologue response2 = new Monologue(localName, "That's sadge :(");
 
-                Choices c = new Choices(localName, "How are you doing my little pog champ?", ChoiceList(Choice("Fine", fine), Choice("Not fine", notFine)));
-                Monologue b = new Monologue("Eyun", "Greetings, stone lover.", c);
-                Monologue a = new Monologue("Karl", "Rock and Stone Leaf Lover", b);
+                Choices c = new Choices(localName, "Eres Eyún Venlen, vas camino a tu trabajo en otro dia de tu rutina, es algo monótona, pero haber buscado tanto trabajo te hace apreciar este, no por la labor como tal, después de todo eres un simple palomero, sino por la compañía, el ambiente en tu oficina es lo mejor, te da un poco de alegría en tu dia.\r\n", ChoiceList(Choice("Fine", response1), Choice("Not fine", response2)));
+                Monologue b = new Monologue("Eyun", "¡Buenos días, come piedras!", c);
+                Monologue a = new Monologue(localName, "¡Buenos días, abraza árboles!", b);
                 return a;
-            case CharacterDialogue.Ivori:
+            case CharacterDialogue.Ivori1:
+                localName = "Ivori";
+                response1 = new Monologue(localName, "That's pog");
+                response2 = new Monologue(localName, "That's sadge :(");
+
+                Choices ivoriC = new Choices(localName, "How are you doing my little pog champ?", ChoiceList(Choice("Fine", response1), Choice("Not fine", response2)));
+                Monologue ivoriB = new Monologue("Eyun", "Greetings, stone lover.", ivoriC);
+                Monologue ivoriA = new Monologue("Karl", "Rock and Stone Leaf Lover", ivoriB);
+                return ivoriA;
+            case CharacterDialogue.Ming1:
                 return null;
-            case CharacterDialogue.Ming:
-                return null;
-            case CharacterDialogue.Kleon:
+            case CharacterDialogue.Kleon1:
                 return null;
             default:
                 return null;
