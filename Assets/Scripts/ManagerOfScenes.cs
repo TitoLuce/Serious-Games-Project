9cd5f79752f558fe0fe3c50035d230332b3aefc0
WittIsHere <59049883+WittIsHere@users.noreply.github.com>
2023-01-22 14:10:45 +0100

    starting cutscenes

---
+++ Assets/Scripts/ManagerOfScenes.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ManagerOfScenes : MonoBehaviour
+{
+
+    public float changeTime1;
+    public string sceneName1;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        changeTime1 -= Time.deltaTime;
+        if (changeTime1 < 0)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName1);
+        }
+    }
+}
